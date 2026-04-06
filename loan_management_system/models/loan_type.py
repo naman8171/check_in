3e0d8a0559126312844b2230cba98eee41f6441a
naman8171 <naman@inomerp.in>
2026-04-06 15:31:27 +0530

    Fix security groups XML for Odoo 19 by removing invalid category field

---
+++ loan_management_system/models/loan_type.py
@@ -0,0 +1,24 @@
+from odoo import fields, models
+
+
+class LoanType(models.Model):
+    _name = "loan.type"
+    _description = "Loan Type"
+    _order = "name"
+
+    name = fields.Char(required=True)
+    code = fields.Char(required=True)
+    active = fields.Boolean(default=True)
+    min_amount = fields.Monetary()
+    max_amount = fields.Monetary()
+    default_interest_rate = fields.Float(help="Annual interest rate in percentage")
+    default_term_months = fields.Integer(default=12)
+    processing_fee_percent = fields.Float(help="Applied on principal amount")
+
+    company_id = fields.Many2one("res.company", default=lambda self: self.env.company, required=True)
+    currency_id = fields.Many2one(related="company_id.currency_id", store=True)
+
+    journal_id = fields.Many2one("account.journal", domain="[('type', 'in', ('bank', 'cash'))]")
+    income_account_id = fields.Many2one("account.account")
+    receivable_account_id = fields.Many2one("account.account")
+    agreement_template = fields.Html()
