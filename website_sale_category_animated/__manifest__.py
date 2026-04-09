29479c359785a2b285c6437d51d970a6e81616eb
Namansaxena568 <namansaxena568@gmail.com>
2026-04-09 16:05:24 +0530

    [third  commit]

---
+++ website_sale_category_animated/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Website Sale Category Animated",
+    "summary": "Premium animated UI and enhanced interaction for website shop categories",
+    "version": "19.0.2.0.0",
+    "category": "Website/eCommerce",
+    "license": "LGPL-3",
+    "author": "Custom",
+    "depends": ["website_sale"],
+    "assets": {
+        "web.assets_frontend": [
+            "website_sale_category_animated/static/src/scss/category_animated.scss",
+            "website_sale_category_animated/static/src/js/category_animated.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
